c898f313451ad11c5796d987f003c138f6edc11e
Yuri Guerra <yuri_guerra@hotmail.com>
2024-08-08 08:29:59 -0300

    adjust .src/config.py

--- src/config/config.py
+++ src/config/config.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker
 class DBConnection:
 
     def ___init__(self) -> None:
-        self.__connection_string = 'mysql_pymysql://root:my-secret-pw@mysqldb/teste'
+        self.connection_string = 'mysql_pymysql://root:my-secret-pw@mysqldb/teste'
         self.session = None
 
     def __enter__(self):
-        engine = create_engine(self.__connection_string)
+        engine = create_engine(self.connection_string)
         session_maker = sessionmaker()
         self.session =  session_maker(bing=engine)
         return self
